a804e3a1351c791bfa70b5af02dc19918ff36cbf
yujinhui <1265921100@qq.com>
2022-06-01 13:56:15 +0800

    test

--- b/b.go
+++
@@ -1,8 +0,0 @@
-package b
-
-import "log"
-
-func Add(x, y int) int {
-	log.Println("You are use package b.Add")
-	return x + y
-}
--- utils.go
+++ utils.go
@@ -1,5 +1,8 @@
 package test
 
+import "log"
+
 func Add(x, y int) int {
+	log.Println("You are using test.Add")
 	return x + y
 }
